dec81c8132745a15dfa2fdf83b55664316d2876b
rchaturvedi <rohitchaturvedi050590@gmail.com>
2021-09-19 11:58:06 -0400

    add webpack config

---
+++ client/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        indent: ['error', 4],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        'react/display-name': 'error',
+    },
+}
